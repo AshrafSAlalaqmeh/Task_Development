06f83ba37c65cfe079501260608f25a45f42415d
ashrafalalaqmeh <a.alaqmeh@globaltribes.com>
2024-10-26 23:42:13 +0300

    backend is done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 const {setupDatabaseSession} = require('./config/database/databaseConfig');
 const app = require("./app");
-const PORT = 3001;
+const PORT = process.env.DEV_PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Api is working fine on port ${PORT}`);
